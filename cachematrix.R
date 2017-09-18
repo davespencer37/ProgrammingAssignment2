32a44d5c3efd4d0c3399a701708f10988bd61dcd
davespencer37 <32061558+davespencer37@users.noreply.github.com>
2017-09-18 11:33:46 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## New to github so just trying to change some comments first
 
 ## Write a short comment describing this function
 
